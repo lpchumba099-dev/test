6519801bafd2486ff07bb595a882890af198578d
Lawrence <lpchumba099@gmail.com>
2026-06-25 18:29:08 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello, lawrence!"
+    return render_template("home.html")
+@app.route("/about")
+def about():
+    return ("about page")
 if __name__=="__main__":
     app.run(debug=True)
